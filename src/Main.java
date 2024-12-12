21d655108e74646dafa3ab9a9c441eb9dab615d5
Gabrielly Boareto <gabrielly.boareto@zup.com.br>
2024-12-12 14:42:59 -0300

    feat: Add Random nunber generation logic

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bem vindo ao Jogo do Adivinha!");
+        System.out.println("Bem-vindo ao Jogo do Adivinha!");
+
+        Random random = new Random();
+        int numeroSorteado = random.nextInt(10) + 1;
+        System.out.println("Número sorteado (teste): " + numeroSorteado);
     }
+
 }
