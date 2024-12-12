655d28e41595a5dfefd9ad06adeb0696905577be
Gabrielly Boareto <gabrielly.boareto@zup.com.br>
2024-12-12 17:42:25 -0300

    feat(game): add method to get user guess for the drawn number

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,7 +7,25 @@ public class Main {
 
         Random random = new Random();
         int numeroSorteado = random.nextInt(10) + 1;
-        System.out.println("Número sorteado (teste): " + numeroSorteado);
+        System.out.println("Número sorteado ): " + numeroSorteado);
+
+        int palpite = obterPalpiteDoUsuario();
+        verificarPalpite(numeroSorteado, palpite);
     }
 
+    public static int   obterPalpiteDoUsuario() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu palpite (1 a 10): ");
+        return scanner.nextInt();
+    }
+
+    public static void verificarPalpite (int numeroSorteado, int palpite) {
+        if (palpite == numeroSorteado) {
+            System.out.println(" Parabéns! Você acertou!");
+        } else if (palpite == numeroSorteado - 1 || palpite == numeroSorteado + 1) {
+            System.out.println("Errou por um! Tente novamente");
+        } else {
+            System.out.println("Errou feio! Tente novamente");
+        }
+    }
 }
